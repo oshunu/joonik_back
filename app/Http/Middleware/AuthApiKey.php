a8c129ebaab4749fc189363303b48d7e427011fc
Oscar Huertas - VPS <oshunu@gmail.com>
2025-01-15 23:25:52 -0600

    comments are added

--- app/Http/Middleware/AuthApiKey.php
+++ app/Http/Middleware/AuthApiKey.php
@@ -9,11 +9,9 @@ use Illuminate\Support\Facades\Log;
 
 class AuthApiKey
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
+    /* 
+        Middleware para validar el API_KEY en las solicitudes. 
+    */
     public function handle(Request $request, Closure $next): Response
     {
         $authorizationHeader = $request->header('Authorization');
@@ -22,8 +20,12 @@ class AuthApiKey
         if ($authorizationHeader && preg_match('/Bearer\s(\S+)/', $authorizationHeader, $matches)) {
             $apiKey = $matches[1];
         }
-        if (!$apiKey || $apiKey !== env('APP_API_KEY')) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if (!$apiKey) {
+            return response()->json(['error' => 'Se Requiere una API_KEY'], 401);
+        }
+
+        if ($apiKey !== env('APP_API_KEY')) {
+            return response()->json(['error' => 'API_KEY Invalida, No autorizado'], 401);
         }
         
         return $next($request);
